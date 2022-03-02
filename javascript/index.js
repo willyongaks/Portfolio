ff0c9342f141083bcf81943e572fe69758fc08c7
willyongaks <william.ongaki10@gmail.com>
2022-03-02 12:58:04 +0100

    n

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,15 @@
-const burger = document.querySelector(".humberger_menu")
+const burger = document.querySelector(".humberger_menu");
+const navLinks = document.querySelectorAll(".nav_links")
 
 
 burger.addEventListener("click", () => {
     document.body.classList.toggle("open")
 
     console.log("got yah!!")
+});
+
+navLinks.forEach(links =>{
+    links.addEventListener("click", () => {
+        document.body.classList.remove("open")
+    });
 });
